10ee10d30a6091bc028023cbbe3b48c43d6724d2
eltons <elton@canway.net>
2018-07-25 21:25:46 +0800

    定义接口

--- src/main/java/net/canway/activiti1/service/WorkflowService.java
+++ src/main/java/net/canway/activiti1/service/WorkflowService.java
@@ -1,11 +1,46 @@
 package net.canway.activiti1.service;
 
-import org.activiti.engine.RepositoryService;
+import java.io.InputStream;
+import java.util.List;
 
 public interface WorkflowService {
 
-    //根据默认的流程图部署流程
-    public String deployDefaultFlow();
+    /**
+     * 根据默认的流程图部署流程
+     */
+    String deployDefaultFlow();
 
-    //根据客户上传的流程图（zip文件，里面须包含流程图bpmn文件）
+    /**
+     * 根据客户上传的流程图（zip文件，里面须包含流程图bpmn文件）
+     */
+    String deployNewFlow();
+
+    /**
+     * 查询部署对象信息
+     */
+    List queryDeployList();
+
+    /**
+     * 查询流程定义信息
+     */
+    List queryDefinitionList();
+
+    /**
+     * 获取部署的流程图片的流
+     */
+    InputStream queryProcessImage();
+
+    /**
+     * 删除部署流程
+     */
+    String delProcessDeploy();
+
+    /**
+     * 删除流程定义
+     */
+    String delProcessDefinition(String processName);
+
+
+
+
 }
--- src/main/java/net/canway/activiti1/service/WorkflowServiceImpl.java
+++ src/main/java/net/canway/activiti1/service/WorkflowServiceImpl.java
@@ -2,8 +2,46 @@ package net.canway.activiti1.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+import java.util.List;
+
 @Service
-public class WorkflowServiceImpl {
+public class WorkflowServiceImpl implements WorkflowService {
 
 
+    @Override
+    public String deployDefaultFlow() {
+
+        return null;
+    }
+
+    @Override
+    public String deployNewFlow() {
+        return null;
+    }
+
+    @Override
+    public List queryDeployList() {
+        return null;
+    }
+
+    @Override
+    public List queryDefinitionList() {
+        return null;
+    }
+
+    @Override
+    public InputStream queryProcessImage() {
+        return null;
+    }
+
+    @Override
+    public String delProcessDeploy() {
+        return null;
+    }
+
+    @Override
+    public String delProcessDefinition(String processName) {
+        return null;
+    }
 }
